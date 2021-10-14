31590e674b965f4724c32740c50d056fa5402ae3
Lorraine Williams <lorrainewilliams2604@gmail.com>
2021-10-14 14:45:04 +0100

    added delete and update functions

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//  Using Mongoose
+
 const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
 const argv = yargs(hideBin(process.argv)).argv;
@@ -10,17 +12,20 @@ const Friend = mongoose.model("Friend",
         name: String
     }
 );
+
 const add = async (name) => {
     const friend = new Friend({ name: name });
     console.log(friend);
     await friend.save();
     process.exit();
 };
+
 const findOne = async (name) => {
     const friend = await Friend.findOne({ name: name });
     console.log(friend)
     process.exit();
 };
+
 const findAll = async () => {
     const friend = await Friend.find();
     console.log(friend);
@@ -31,7 +36,39 @@ const findSome = async (name) => {
     console.log(friend);
     process.exit();
 };
+
+const updateOne = async (name, newName) => {
+    const friend = await Friend.updateOne({ name: name }, { name: newName });
+    console.log(friend);
+    process.exit();
+};
+
+const updateMany = async (name, newName) => {
+    const friend = await Friend.updateMany({ name: name }, { name: newName });
+    console.log(friend);
+    process.exit();
+};
+
+const deleteOne = async (name) => {
+    const friend = await Friend.deleteOne({ name: name });
+    console.log(friend);
+    process.exit();
+};
+
+const deleteMany = async (name) => {
+    const friend = await Friend.deleteMany({ name: name });
+    console.log(friend);
+    process.exit();
+};
+
+const deleteAll = async () => {
+    const friend = await Friend.deleteMany();
+    console.log(friend);
+    process.exit();
+};
+
 const main = async (argv) => {
+    console.log(argv)
     try {
         if (argv.add) {
             await add(argv.name)
@@ -41,6 +78,16 @@ const main = async (argv) => {
             await findAll();
         } else if (argv.findSome) {
             await findSome(argv.name)
+        } else if (argv.updateOne) {
+            await updateOne(argv.name, argv.newName)
+        } else if (argv.updateMany) {
+            await updateMany(argv.name, argv.newName);
+        } else if (argv.deleteOne) {
+            await deleteOne(argv.name);
+        } else if (argv.deleteMany) {
+            await deleteMany(argv.name)
+        } else if (argv.deleteAll) {
+            await deleteAll(argv.name)
         }
     } catch (error) {
         console.log(error)
